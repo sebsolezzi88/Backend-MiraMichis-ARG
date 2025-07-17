b05e3be29262595599d83b1a968704fe4f9ec617
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-17 20:38:17 -0300

    Funcionalidad para loguearse

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { validateUserRegister } from "../middlewares/validateUserRegister";
-import { registerUser } from "../controllers/userController";
+import { activateAccount, registerUser } from "../controllers/userController";
 
 const router = Router();
 
 router.post('/register',validateUserRegister ,registerUser);
+router.get('/activate',activateAccount);
 
 export default router;
