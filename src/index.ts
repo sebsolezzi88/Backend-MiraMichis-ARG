f44e492fe5a72c5f08b5ea8f7b49f2f96d11974c
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-17 20:26:03 -0300

    Configuracion de index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import "./models/loadModels";
+import { getMongoConnection } from "./config/db";
+
+//Cargar variable de entorno
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json()); //Para leer los json
+
+getMongoConnection(); //Conexion con la base de datos
+
+app.listen(PORT, ()=>{
+    console.log(`Server Express corriendo en puerto ${PORT}`);
+})
