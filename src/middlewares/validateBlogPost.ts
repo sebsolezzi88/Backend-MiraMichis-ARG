fa96115176fad4c81323534eb926c33c97118a3c
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-18 10:34:16 -0300

    Añadiendo validacion de URL

--- src/middlewares/validateBlogPost.ts
+++ src/middlewares/validateBlogPost.ts
@@ -9,6 +9,10 @@ export const validateBlogPost = [
 
   body('typeOfBlogPost')
     .notEmpty().withMessage('typeOfBlogPost is required')
-    .isIn(["noticia", "evento", "salud", "educación", "video"]).withMessage('The post type is not valid. The allowed options are:noticia, evento, salud, educación, video')
+    .isIn(["noticia", "evento", "salud", "educación", "video"]).withMessage('The post type is not valid. The allowed options are:noticia, evento, salud, educación, video'),
+    
+  body('link')
+    .optional({ checkFalsy: true }) 
+    .isURL().withMessage('The link must be a valid URL') 
 
 ];
