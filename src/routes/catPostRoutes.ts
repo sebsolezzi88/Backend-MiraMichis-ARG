8bd2ab8ee2250aaa4e58d2ba5071546310555893
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-17 22:29:35 -0300

    Actualizar status

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { createCatPost, deleteCatPostById, getAllCatPosts, getCatPostById } from "../controllers/catPostController";
+import { createCatPost, deleteCatPostById, getAllCatPosts, getCatPostById, updateCatPostStatus } from "../controllers/catPostController";
 import { verifyToken } from "../middlewares/authMiddleware";
 import uploadImage from "../middlewares/uploadImage";
 import { AuthRequest } from "../types/express";
@@ -11,6 +11,7 @@ const router = Router();
 router.get('/',getAllCatPosts); //Obtener todos los post
 router.get('/:id', getCatPostById);   // Obtener un catpost por ID
 router.delete('/:id',verifyToken ,deleteCatPostById);      // Eliminar un catpost por ID
+router.patch('/:id/status',verifyToken ,updateCatPostStatus);//Actualizar estado activo/resuelto
 router.post('/',verifyToken,
     uploadImage.single('photo'),(req: Request, res: Response, next: NextFunction) => {
     // TypeScript sabe que 'req' es 'Request' aquí.
