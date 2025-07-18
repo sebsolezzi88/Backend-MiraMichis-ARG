2b0e079acbae05fa4d9f6085e52b070b0919203c
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-18 10:26:50 -0300

    validador para crear blogpost

---
+++ src/middlewares/validateBlogPost.ts
@@ -0,0 +1,14 @@
+import { body } from 'express-validator';
+
+export const validateBlogPost = [
+  body('title')
+    .notEmpty().withMessage('title is required'),
+
+  body('text')
+    .notEmpty().withMessage('text is required'),
+
+  body('typeOfBlogPost')
+    .notEmpty().withMessage('typeOfBlogPost is required')
+    .isIn(["noticia", "evento", "salud", "educación", "video"]).withMessage('The post type is not valid. The allowed options are:noticia, evento, salud, educación, video')
+
+];
